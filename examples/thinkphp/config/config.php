060152c717100e95805d9b0459ec146838cb83c1
李铭昕 <715557344@qq.com>
2020-08-27 16:50:15 +0800

    Added config for aop.

---
+++ examples/thinkphp/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
+
+return [
+    'annotations' => [
+        'scan' => [
+            'paths' => [
+                BASE_PATH . '/app',
+            ],
+            'ignore_annotations' => [
+                'mixin',
+            ],
+            'class_map' => [
+            ],
+        ],
+    ],
+];
